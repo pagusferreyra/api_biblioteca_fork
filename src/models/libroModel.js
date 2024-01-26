1983b20b97ae48c872f89f9156e257be720d5059
pagusferreyra <pagusferreyra@gmail.com>
2024-01-26 11:11:39 -0300

    Tests Unitarios y de Integración

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB, {
+mongoose.connect('mongodb://localhost:27017/biblioteca', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
